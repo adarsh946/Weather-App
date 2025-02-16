90ebbbee465c30cb43b42f430280aab298620596
adarsh946 <adarsh.shukla2724@gmail.com>
2025-02-16 08:37:04 +0530

    added geolocation hook

---
+++ src/hooks/useGeolocation.ts
@@ -0,0 +1,79 @@
+import type { Coordinates } from "@/config/types";
+import { useEffect, useState } from "react";
+
+interface geoLocationData {
+  coordinates: Coordinates | null;
+  error: string | null;
+  isLoading: boolean;
+}
+
+const useGeoLocation = () => {
+  const [locationData, setLocationData] = useState<geoLocationData>({
+    coordinates: null,
+    error: null,
+    isLoading: false,
+  });
+
+  const geoLocation = () => {
+    if (!navigator.geolocation) {
+      setLocationData({
+        coordinates: null,
+        error: "Your Location data not found by the browser.",
+        isLoading: true,
+      });
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocationData({
+          coordinates: {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          },
+          error: null,
+          isLoading: false,
+        });
+      },
+
+      (error) => {
+        let errorMessage: string;
+
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            errorMessage = "location permission is denied";
+            break;
+          case error.POSITION_UNAVAILABLE:
+            errorMessage = "location information is unavalable";
+            break;
+          case error.TIMEOUT:
+            errorMessage = "Location request Timed out";
+            break;
+          default:
+            errorMessage = "An unknown error occured";
+            break;
+        }
+
+        setLocationData({
+          coordinates: null,
+          error: errorMessage,
+          isLoading: false,
+        });
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  };
+
+  // get location data when component mounts
+  useEffect(() => {
+    geoLocation();
+  }, []);
+
+  return {
+    ...locationData,
+    geoLocation, // method to manully refresh location
+  };
+};
