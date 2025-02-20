da16cd361a1f19e6fb144e2137f210b852f830ef
adarsh946 <adarsh.shukla2724@gmail.com>
2025-02-20 17:05:45 +0530

    added suggestions in seach cities

--- src/components/SearchCity.tsx
+++ src/components/SearchCity.tsx
@@ -7,15 +7,24 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
+  CommandSeparator,
 } from "./ui/command";
 import { Button } from "./ui/button";
-import { Search } from "lucide-react";
+import { Loader2, Search } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function SearchCity() {
   const [query, setQuery] = useState<string>("");
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
 
   const { data: location, isLoading } = useCityLocationQuery(query);
+
+  const handleSelect = (cityData: string) => {
+    const [lat, lon, name, country] = cityData.split("|");
+    setOpen(false);
+    navigate(`/city/?at=${lat}&lon=${lon}`);
+  };
 
   return (
     <>
@@ -28,14 +37,51 @@ function SearchCity() {
         Search cities...
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Search cities..." />
+        <CommandInput
+          placeholder="Search cities..."
+          value={query}
+          onValueChange={setQuery}
+        />
         <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
+          {query.length > 2 && !isLoading && (
+            <CommandEmpty>No cities found.</CommandEmpty>
+          )}
+
+          <CommandGroup heading="Favouraites">
             <CommandItem>Calendar</CommandItem>
-            <CommandItem>Search Emoji</CommandItem>
-            <CommandItem>Calculator</CommandItem>
           </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Recent Searches">
+            <CommandItem>Calendar</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+
+          {location && location.length > 0 && (
+            <CommandGroup heading="Suggestions">
+              {isLoading && (
+                <div className="flex justify-center items-center p-4">
+                  <Loader2 className="h-4 w-4" />
+                </div>
+              )}
+              {location.map((locations) => (
+                <CommandItem
+                  value={`${locations.lat} | ${locations.lon} | ${locations.name} | ${locations.country}`}
+                  onSelect={handleSelect}
+                >
+                  <Search className=" mr-2 h-4 w-4 gap-3" />
+                  <span>{locations.name}</span>
+                  {locations.state && (
+                    <span className="text-sm text-muted-foreground">
+                      , {locations.state}
+                    </span>
+                  )}
+                  <span className="text-sm text-muted-foreground">
+                    , {locations.country}
+                  </span>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
         </CommandList>
       </CommandDialog>
     </>
